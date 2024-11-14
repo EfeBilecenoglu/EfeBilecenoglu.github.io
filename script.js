f2a97ac2ebc62cd8e436bb21eb38d1130cbdf0f8
Ahmet Efe Bilecenoğlu <168021085+EfeBilecenoglu@users.noreply.github.com>
2024-11-15 02:38:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -56,34 +56,33 @@ document.addEventListener("DOMContentLoaded", function() {
 
 //Slide Show
 
-let slideIndex = 0; // İlk slayt başlasın
-        showSlides();
+let slideIndex = 0; // Başlangıçtaki slayt indexi
 
-        // Show slides function
-        function showSlides() {
-            let slides = document.getElementsByClassName("mySlides");
+// Slaytları gösterme işlevi
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+    
+    if (slideIndex > slides.length) {
+    slideIndex = 0;
+  // Tüm slaytları gizle
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
 
-            if (slideIndex > slides.length) {
-        slideIndex = 0; // Döngüye girmesi için
-    }
-            // Tüm slaytları gizle
-            for (let i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
+  // Sonraki slayda geç
+  slideIndex++;
 
+  // Geçerli slaydı göster
+  slides[slideIndex - 1].style.display = "block";
+    // Eğer slaytlar biterse, başa dön
+  
+  }
+}
 
-            // Şu anki slaytı göster
-            slides[slideIndex].style.display = "block";
+// Bir sonraki veya önceki slayda geçiş yapma
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-            // Otomatik geçiş için 10 saniye sonra bir sonraki slaytı göster
-            slideIndex++;
-
-           
-        }
-         setTimeout(showSlides, 10000); // 10 saniye
-
-        // Next/Prev button control
-        function plusSlides(n) {
-            
-            showSlides(slideIndex += n;);
-        }
+// Otomatik slayt geçişi
+setInterval(showSlides, 10000); // 10 saniyede bir slayt değişir
